66c352ef0ab1797ccef4bfefbaecb8712945d5bf
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-05-28 19:47:18 -0400

    feat: component footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-6">
+            <p>&copy; 2024 Zézim</p>
+          </div>
+          <div className="col-md-6">
+            <p>
+              <a href="#">Voltar ao topo</a>
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
