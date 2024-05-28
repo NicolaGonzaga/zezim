12d7d83984a0c5f73a033fd552fb6988f3f5335b
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-05-28 19:46:33 -0400

    feat: component header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="container">
+        <img src="logo.png" alt="Logo" className="logo" />
+        <p>Zézim</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
