4f8f80173e785f69463bc9c0e805eff59d5c597d
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-05-31 14:08:34 -0400

    feat: Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,32 +1,63 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./Home.css";
-import Header from "../Header/Header";
-import Footer from "../Footer/Footer";
 
 function Home() {
-  const [address, setAddress] = useState("");
+  const [endereco, setEndereco] = useState("");
+  const [erro, setErro] = useState("");
+  const navigate = useNavigate();
+
+  const handleSearch = () => {
+    if (!endereco.trim()) {
+      setErro("Por favor, digite seu endereço.");
+      return;
+    }
+
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({ endereco }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setEndereco(data);
+        navigate("/products", { state: { endereco: data.endereco } });
+      })
+      .catch((error) => {
+        console.error(
+          "Ocorreu um erro ao fazer a requisição para a API:",
+          error
+        );
+      });
+  };
+
+  const handleInputChange = (event) => {
+    setEndereco(event.target.value);
+    if (event.target.value.trim()) {
+      setErro("");
+    }
+  };
 
   return (
-    <div className="home">
-      <Header />
-      <main>
-        <section>
-          <div className="content">
-            <h1>Bebida rápida, gelada e no precinho? </h1>
-            <h2>O Zézim entrega tudo.</h2>
-            <input
-              type="text"
-              placeholder="Digite seu endereço"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              className="form-control"
-            />
-            <button>Buscar Produtos</button>
-          </div>
-        </section>
-      </main>
-      <Footer />
-    </div>
+    <main>
+      <div className="hero">
+        <h1>Bebida rápida, gelada e no precinho? </h1>
+        <h2>O Zézim entrega tudo.</h2>
+        <div className="content">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Digite seu endereço"
+            value={endereco}
+            onChange={handleInputChange}
+          />
+          <button onClick={handleSearch}>Buscar Produtos</button>
+          {erro && <p className="error">{erro}</p>}
+        </div>
+      </div>
+    </main>
   );
 }
 
