341d043a88e78399ac9cdfe00505980dc7e4c966
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-05-31 14:07:59 -0400

    chore : update Footer

---
+++ src/components/Layout/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <p>
+          <a href="#">Voltar ao topo</a>
+        </p>
+        <p>&copy; 2024 Zézim</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
