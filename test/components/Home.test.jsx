9afec0cf7e4ae1875b2116afcbad9310e35c307b
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-06-02 13:32:10 -0400

    test: updating home test

--- test/components/Home.test.jsx
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect, screen } from "vitest";
-import Home from "../../src/components/page/home/Home";
-
-describe("Home", () => {
-  it("should render correctly", () => {
-    expect(<Home />);
-  });
-});
