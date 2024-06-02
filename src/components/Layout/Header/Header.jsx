648db93643cb2a09600426b1a7c13dedc1833832
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-06-02 10:37:26 -0400

    feat: updating component

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -2,20 +2,14 @@ import { Link, useLocation } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
-  const location = useLocation();
-
   return (
     <header className="header">
       <img src="logo.png" alt="Logo" className="logo" />
-      {location.pathname === "/products" ? (
-        <>
-          <Link to="/" className="text-router">
-            Zézim, o aumigão da gente.
-          </Link>
-        </>
-      ) : (
-        <p>Zézim, o aumigão da gente.</p>
-      )}
+      <>
+        <Link to="/" className="text-router">
+          Zézim, o aumigão da gente.
+        </Link>
+      </>
       <button className="button">Entrar</button>
     </header>
   );
