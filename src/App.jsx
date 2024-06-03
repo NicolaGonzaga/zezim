67735761032e6b3385e78cfcff667f8af8de8264
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-06-03 12:49:32 -0400

    fix: update import

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import Home from ".";
 import Products from "./components/page/products/Products";
+import Home from "./components/page/home/Home";
 import Header from "./components/layout/header/Header";
 import Footer from "./components/layout/footer/Footer";
 
