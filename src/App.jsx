2df11dac2aeac111e4507c2db0ded73a067936f5
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-06-03 13:13:50 -0400

    fix: fixed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import Products from "./components/page/products/Products";
-import Home from "./components/page/home/Home";
-import Header from "./components/layout/header/Header";
-import Footer from "./components/layout/footer/Footer";
+import Header from "./components/Layout/Header/Header";
+import Footer from "./components/Layout/Footer/Footer";
+import Home from "./components/Page/Home/Home";
+import Products from "./components/Page/Products/Products";
 
 function App() {
   return (
