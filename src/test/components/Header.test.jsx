49204bc78a9cd2ee2ef4db396d8b47f9a12e2551
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-06-03 14:08:55 -0400

    fix: fixed import

--- src/test/components/Header.test.jsx
+++ src/test/components/Header.test.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import { MemoryRouter } from "react-router-dom";
 import { describe, it, expect } from "vitest";
-import Header from "../../components/layout/header/Header";
+import Header from "../../components/Layout/Header/Header";
 
 describe("Header", () => {
   it("renders the logo correctly", () => {
