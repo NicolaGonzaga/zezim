a0e2d9e4300f656baf6fd82d8254e7de964cef40
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-06-03 22:09:06 -0400

    fix: fixed global responsive styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,18 @@ import Header from "./components/Layout/Header/Header";
 import Footer from "./components/Layout/Footer/Footer";
 import Home from "./components/Page/Home/Home";
 import Products from "./components/Page/Products/Products";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <div className="layout">
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
       </Routes>
       <Footer />
-    </>
+    </div>
   );
 }
 
