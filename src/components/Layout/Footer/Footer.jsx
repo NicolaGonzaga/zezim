327f4a5614432be57be44e429e1e67defa58198a
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-06-04 09:04:22 -0400

    chore: added dinamic year footer

--- src/components/Layout/Footer/Footer.jsx
+++ src/components/Layout/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import "./Footer.css";
 
 function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="footer">
       <div className="container-footer">
-        <p>&copy; 2024 Zézim</p>
+        <p>&copy; {currentYear} Zézim</p>
       </div>
     </footer>
   );
