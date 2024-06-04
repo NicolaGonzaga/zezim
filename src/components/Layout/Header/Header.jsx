6ab2c246ec2e1daff5416dd89bc72a80255b7d8a
Nicola Gonzaga <nicolagonzaga@gmail.com>
2024-06-03 22:05:44 -0400

    chore: update import

--- src/components/Layout/Header/Header.jsx
+++ src/components/Layout/Header/Header.jsx
@@ -1,11 +1,11 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 function Header() {
   return (
     <header className="header">
       <Link to="/">
-          <img src="logo.png" alt="Logo" className="logo" />
+        <img src="logo.png" alt="Logo" className="logo" />
       </Link>
       <p>Zézim, o aumigão da gente.</p>
       <button className="button">Entrar</button>
